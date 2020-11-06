50c1746210f30dcb232072f3bb8e27aecb671eda
cokoyoh <charlesokoyoh@gmail.com>
2020-11-06 22:50:37 +0300

    Manipulating the dom day-2

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-alert(location);
+// alert(location);
 
-if (confirm('Go to Wikipedia')) {
-  location = 'https://wikipedia.org'; 
-} 
+// if (confirm('Go to Wikipedia')) {
+//   location = 'https://wikipedia.org'; 
+// } 
+
+document.body.style.background = 'red';
+
+// clear after 3 seconds
+setTimeout(() => document.body.style.background = '', 5000);
