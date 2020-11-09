f5c8937911274fdddcc58fbb751d541da882c164
cokoyoh <charlesokoyoh@gmail.com>
2020-11-09 22:32:50 +0300

    Dom selection and manipulation using console

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ document.body.style.background = 'red';
 
 // clear after 3 seconds
 setTimeout(() => document.body.style.background = '', 5000);
+
+/**
+ * Notes
+ *  the last selected DOM element is available at $0, the previously selected is $1
+ */
+$0.style.background = 'red'; //changes the last element's background to red
